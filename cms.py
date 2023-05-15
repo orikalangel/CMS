fee6851e61888449323e1bad92fd8744559ad57a
orikalangel <orikalangel@gmail.com>
2023-05-15 10:47:59 +0300

    first program

---
+++ cms.py
@@ -0,0 +1,10 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+#camera.rotation=180
+
+camera.start_preview()
+sleep(52)
+camera.capture('/home/st-001/Documents/1.jpg')#if "1%s.jpg %i" replace "1.jpg" that chance tha name of the file - in loop
+camera.stop_preview()
